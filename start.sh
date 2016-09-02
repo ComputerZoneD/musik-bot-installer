e99f3b2efed4fe2bb37251ad438c05f9a8fe3ecb
Blacktiger <lpzockerrmc@gmail.com>
2016-09-02 19:50:20 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,18 @@
-wget http://file.server-space.online/news.txt >/dev/null 2>&1
-wget http://file.server-space.online/1029.txt >/dev/null 2>&1
+wget http://file.server-space.online/news.txt
+wget http://file.server-space.online/1029.txt
+clear
 chmod 777 news.txt
 echo "Versions Check...."
 sleep 3
-grep palita < Version.txt
+cat Version.txt
 sleep 3
-grep palita < news.txt
+cat < news.txt
 sleep 5
-grep palita < 1029.txt
+clear
+cat < 1029.txt
 sleep 5
 clear
 echo "Sollte deine Version alt sein dann installiere den Bot per script auf meiner github seite neu LG Blacktiger"
 sleep 1
-rm -f 1029.txt news.txt 
+rm -f 1029.txt news.txt
 screen -dmS ts3bot ./sinusbot -RunningAsRootIsEvilAndIKnowThat
